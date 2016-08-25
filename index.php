a3df3f2592619dfeb22db686b5a2cd838a5669d9
Ping-Yen Tsai <pingyen@gmail.com>
2016-08-25 11:54:19 +0800

    Added missing doctype

--- index.php
+++ index.php
@@ -53,6 +53,7 @@
 
 	ob_start();
 ?>
+<!DOCTYPE html>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1" >
--- taipei.php
+++ taipei.php
@@ -71,6 +71,7 @@
 
 	ob_start();
 ?>
+<!DOCTYPE html>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1" >
